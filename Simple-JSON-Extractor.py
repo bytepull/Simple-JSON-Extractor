ca89772ebf6314e90653f75d029c26d7fcb453ea
Luca Giommoni <33961603+lucagiommoni@users.noreply.github.com>
2021-05-16 01:25:11 +0200

    Add files via upload

---
+++ Simple-JSON-Extractor.py
@@ -0,0 +1,42 @@
+import sys, os, json, re
+
+inputFile = open(sys.argv[1])
+pattern = re.compile(r'\{[^}]+\}')
+gCounter = 0
+path = os.path.join(os.getcwd(),'Simple-JSON-Extractor')
+os.mkdir(path)
+
+def fn1(inputFile):
+  obj = ''
+  flag = False
+  counter = 0
+  global gCounter
+  
+  for l in inputFile:
+    for c in l:
+      if c == "{": 
+        counter += 1
+        flag = True
+      elif c == "}": counter -= 1
+      if flag: obj += c
+      if counter == 0 and flag:
+        flag = False
+        ft = obj.replace("'", "\"").replace(",}", "}")
+        obj = ''
+        if len(ft) == 2: continue
+        try:
+          jsonproof = json.dumps(json.loads(ft), indent=2)
+          gCounter += 1
+          outputFile = open(os.path.join(path, 'object_' + str(gCounter)) + '.json', 'w')
+          outputFile.write(str(jsonproof))
+          outputFile.close()
+        except Exception as e:
+          # print(e)
+          # print(ft)
+          if len(pattern.findall(ft)) > 1:
+            fn1(ft[1:len(ft)-1])
+
+fn1(inputFile)
+inputFile.close()
+print(str(gCounter) + " JSON object found!")
+
